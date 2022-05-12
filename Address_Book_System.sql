d140e511a03aea30ccc07e36e127b5fe53cfb411
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 02:24:27 +0530

    UC6-RetriveValueBasedOnCity

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,4 +1,6 @@
-select * 
-from Address_Book_Tables1
-where City='Devrukh' or State='Ratnagiri'
-Select * from Address_Book_Tables1;
+------ UC 7: Ability to Retrieve Count of Person belonging to a City or State ------
+Insert into Address_Book_Tables1(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email) 
+values('meena','Vargheese','22 Catherine Street','thane','Mumbai',2545001,777343443,'Meena@gmail.com')
+select Count(*),state,City
+from Address_Book_Table
+Group by state,City
