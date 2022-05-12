91859db2fa8ada85b0eac69fb112e1468fa67e1a
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 02:15:06 +0530

    UC5-DeleteValueBasedOnName

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,5 +1,5 @@
-Update Address_Book_Tables1
-set Email='shindeomkar.sh@gmail.com'
-where FirstName='omkar'
-
+------ UC 5: Ability to Delete Contact Person Based on their Name ------
+delete 
+from Address_Book_Tables1
+where FirstName='bunty' and SecondName='shinde'
 Select * from Address_Book_Tables1;
