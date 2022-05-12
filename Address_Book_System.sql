738fc10121384a5bba963cf157e0d01bc4c89a19
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 02:41:04 +0530

    UC11-ER-Daigram

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,7 +1,15 @@
-select Count(*) as NumberOfContacts,Type
-from Address_Book_Tables1
-Group by Type
+select Address_BookName,Concat(FirstName,' ',SecondName) as Name,Concat(Address,' ,',City,' ,',State,' ,',zip) as Address,PhoneNumber,Email,ContactType_Name
+from Address_Book 
+Full JOIN Contact_Person on Address_Book.Address_BookID=AddressBook_ID 
+Full JOIN TypeManager on TypeManager.Contact_Identity=Contact_ID
+Full JOIN ContactType on TypeManager.ContactType_Identity=ContactType_ID
 
-update Address_Book_Tables1
-set AddressBookName='Mom',Type='Family'
-where FirstName='omkar'
+------  UseCase 12------  
+
+-- 1: Ability to Retrieve Person belonging to a City or State --
+
+select Address_BookName,Concat(FirstName,' ',SecondName) as Name,Concat(Address,' ,',City,' ,',State,' ,',zip) as Address,PhoneNumber,Email,ContactType_Name
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.Address_BookID=AddressBook_ID and (City='Devrukh' or State='Ratnagiri')
+INNER JOIN TypeManager on TypeManager.Contact_Identity=Contact_ID
+INNER JOIN ContactType on TypeManager.ContactType_Identity=ContactType_ID
