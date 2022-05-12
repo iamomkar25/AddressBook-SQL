f50ae87f769a9a2e77472dafba90825a6b16b8c8
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 02:12:10 +0530

    UC4-EditValueBasedOnName

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,6 +1,5 @@
+Update Address_Book_Tables1
+set Email='shindeomkar.sh@gmail.com'
+where FirstName='omkar'
 
-Insert into Address_Book_Tables1(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email) 
-values('omkar','shinde','645 ','Devrukh','Ratnagiri',415804,7774962478,'omkar25shinde@gmail.com'),
-('bunty','shinde','836','talegoan','Pune',410507,8884962478,'bunty@gmail.com');
-
-select * from Address_Book_Tables1;
+Select * from Address_Book_Tables1;
