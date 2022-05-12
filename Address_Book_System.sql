089664fdde1759851e9f542a2a5e9fc92882890d
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 02:06:59 +0530

    insertValue

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,14 +1,6 @@
 
------ UC 2: Create Table ------
-create table Address_Book_Tables1
-(FirstName varchar(100),
-SecondName varchar(100),
-Address varchar(250),
-City varchar(100),
-State varchar(100),
-zip BigInt,
-PhoneNumber BigInt,
-Email varchar(200)
-)
+Insert into Address_Book_Tables1(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email) 
+values('omkar','shinde','645 ','Devrukh','Ratnagiri',415804,7774962478,'omkar25shinde@gmail.com'),
+('bunty','shinde','836','talegoan','Pune',410507,8884962478,'bunty@gmail.com');
 
 select * from Address_Book_Tables1;
