81e5fa2fe4e56925c596ae4c3627578cc41c9ece
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 02:26:59 +0530

    UC8-sort

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,6 +1,6 @@
------- UC 7: Ability to Retrieve Count of Person belonging to a City or State ------
-Insert into Address_Book_Tables1(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email) 
-values('meena','Vargheese','22 Catherine Street','thane','Mumbai',2545001,777343443,'Meena@gmail.com')
-select Count(*),state,City
-from Address_Book_Table
-Group by state,City
+------ UC 8: Ability to retrieve entries sorted alphabetically ------
+
+select *
+from Address_Book_Tables1
+where City='Devrukh'
+order by(FirstName)
